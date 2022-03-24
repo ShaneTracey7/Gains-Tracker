e8017067a74049f9cf6b8543dec2f17e5e38c505
Shane Tracey <71601674+ShaneTracey7@users.noreply.github.com>
2022-03-23 23:44:11 -0400

    Update createAccount.php

--- createAccount.php
+++ createAccount.php
@@ -4,7 +4,6 @@
 Final Project (Individual)
 Class: Advanced Website Design
 Name: Shane Tracey
-Student ID: 105076627
 Due: August 4th, 2021
 create account page 
 -->       
@@ -107,9 +106,9 @@ create account page
 
 /* Connecting to DB */
 $servername = "localhost";
-$usernamedb = "traceyu_awd"; 
-$passworddb= "awdfinalp";
-$dbname = "traceyu_awdfinalp";
+//$usernamedb = *insert username for database*; 
+//$passworddb= *insert password for database*;
+//$dbname = *insert name of database*;
 
 //form has successfully been submitted
 if(!empty($_POST["fn"]))
